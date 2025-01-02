f5e4f786e426bc8ad6be44a3e0d25c1e3888eff0
soorajydv <soorajydv9@gmail.com>
2025-01-02 09:57:35 +0545

    fix payment route

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 require('dotenv').config();
 
-const fonepayRouter = require('./routes/payment');
+const fonepayRouter = require('./src/routes/payment');
 
 app.use(express.json());
 
